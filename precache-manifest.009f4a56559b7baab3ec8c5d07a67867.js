0a4657984245fdb0d108242e1778a2ca7ae798ba
gloomykevin <1293535916@qq.com>
2020-10-08 18:17:18 +0800

    Updates

---
+++ precache-manifest.009f4a56559b7baab3ec8c5d07a67867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d571a0a48458d67734b93bf2df46f80a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69f50e6a398c02e196b3",
+    "url": "./static/css/main.32c53f69.chunk.css"
+  },
+  {
+    "revision": "71fb58979024fa05b809",
+    "url": "./static/js/2.93bec7cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.93bec7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f50e6a398c02e196b3",
+    "url": "./static/js/main.88811bb6.chunk.js"
+  },
+  {
+    "revision": "2cebae45e93442002eab44341159cddb",
+    "url": "./static/js/main.88811bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42113c31a7cc852ba57",
+    "url": "./static/js/runtime-main.060c06ee.js"
+  }
+]);
